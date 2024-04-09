65dd39f24829cb820d2b6f53dc2bb37d531b62e9
alexDanilMain <alexdanilwork@gmail.com>
2024-04-09 09:23:06 +0200

    adds NavBar

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -3,7 +3,13 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+        <nav className="flex h-14 z-40 w-full items-center justify-start sticky top-0 inset-x-0 bg-gradient-to-r from-[rgb(255,121,97)] to-[rgb(243,92,126)] py-1">
+            <div className="container mx-auto">
+              <p className="text-xl">&lt; Salt / &gt;</p>
+            </div>
+        </nav>
+
+
     </>
   )
 }
