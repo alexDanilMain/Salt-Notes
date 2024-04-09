df921766d246cb50a9bbc21b5bf60a2fbace3dec
alexDanilMain <alexdanilwork@gmail.com>
2024-04-09 19:13:05 +0200

    adds dropdown logic

--- FrontEnd/src/components/SideBar/Days.tsx
+++ FrontEnd/src/components/SideBar/Days.tsx
@@ -5,7 +5,7 @@ type Props = {
 function Days({ number }: Props) {
     return (
         <li>
-            <a href="#" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Day {number}</a>
+            <a href="#" className="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:hover:bg-gray-200">Day {number}</a>
         </li>
     )
 }
