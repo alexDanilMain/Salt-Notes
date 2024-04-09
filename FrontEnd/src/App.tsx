eb9f52838c3416d968ffbd818a358d417e1d24a6
alexDanilMain <alexdanilwork@gmail.com>
2024-04-09 19:48:38 +0200

    adds skeleton for create notes

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -10,10 +10,17 @@ function App() {
             </div>
         </nav>
 
-        <section className="flex w-full mx-auto relative flex-col">
+        <section className="flex w-full mx-auto relative flex-col sm:flex-row">
           <Sidebar />
-          <div className="w-full h-64 bg-black">
-            
+          <div className="flex-1 h-96 ml-64 border-2 border-black">
+              <div className="w-full flex justify-end items-center gap-4 pr-4 bg-gray-50 border-b border-2 shadow-md h-16">
+                <button className="h-8 rounded-full bg-gray-200 hover:bg-gray-300 py-1 px-2"> Cancel Changes </button>
+                <button className="h-8 rounded-full bg-green-400 hover:bg-green-500 py-1 px-2"> Commit Changes </button>
+              </div>
+
+              <div className="w-4/5 mx-auto mt-4">
+                <textarea className="w-full resize-none outline-none bg-gray-200 rounded-md p-4" name="" id="" cols={30} rows={10}></textarea>
+              </div>
           </div>
         </section>
     </>
