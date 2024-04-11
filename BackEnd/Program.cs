574015bb4111f44f31d944c87ef96825f93c6d85
alexdanilwork <alexdanilwork@gmail.com>
2024-04-11 11:44:04 +0200

    adds authorization to backend

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,20 +1,56 @@
+using backend.Config;
 using BackEnd.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddAuthorization();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Add services to the container.
 
+var JwtSecurityScheme = new OpenApiSecurityScheme()
+{
+    Type = SecuritySchemeType.Http,
+    Scheme = "bearer"
+};
+var JwtSecurityRequirement = new OpenApiSecurityRequirement()
+{
+    [new() { Reference = new() { Type = ReferenceType.SecurityScheme, Id = "Bearer" } }] = []
+};
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", JwtSecurityScheme);
+    options.AddSecurityRequirement(SecurityConfig.JwtSecurityRequirement);
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "*",
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
+
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.Authority = "https://accounts.google.com";
+    options.Audience = "887329743818-i7f0mmne75n5ibn7khdq387ivk6s6en2.apps.googleusercontent.com";
+});
 
 var app = builder.Build();
+app.UseCors(x => x
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(origin => true) // allow any origin
+                                                        //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins separated with comma
+                    .AllowCredentials());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,15 +58,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-
 app.UseHttpsRedirection();
 
-app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79744aada753ac66de66872573fb79667c48ccec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a0c19aa25410e1fade817730094c5fd925ef15")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
