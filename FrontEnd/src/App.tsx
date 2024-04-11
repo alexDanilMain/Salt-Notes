1bc55f373a31847bde2ecbc4d069d7949fa9c5aa
alexdanilwork <alexdanilwork@gmail.com>
2024-04-11 11:11:39 +0200

    creates automatic login

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import GoogleLogIn from "./components/GoogleLogin/GoogleLogin";
 import NavBar from "./components/NavBar/NavBar";
 import SaltyNote from "./imgs/SaltyNote.png";
+import { getCookie } from "./services/Cookies/Cookies";
+import { useNavigate } from "react-router-dom";
 function App() {
+  const navigate = useNavigate();
   
+  useEffect(() => {
+    let Token = getCookie("saltnote_key");
+    if (typeof Token == "string") {
+        navigate("/home")
+    }
+  }, []);
+
   return (
+
     <main className="bg-gray-50 h-screen">
       <NavBar />
       <section className="flex w-full mx-auto relative flex-col justify-center items-center h-full gap-12">
